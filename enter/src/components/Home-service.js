9818f7fd0a0e4d6549452f13f7cad706b7076f03
yjbae3974 <49051494+yjbae3974@users.noreply.github.com>
2021-10-05 18:22:46 +0900

    home service 거의 완료

--- enter/src/components/Home-service.js
+++ enter/src/components/Home-service.js
@@ -4,13 +4,13 @@ function HomeService(){
     return(
         <>
                 <div className="container">
-				<div>
+				<div className="p-relative">
 				  <div className="dot-4"></div>
 				  <div className="dot-5"></div>
 				  <div className="dot-6"></div>
-				  <div>
-					  <div className="row d-flex align-items-center">
-						  <div className="col-lg-6">
+				  <div className="company-top">
+					  <div className="row d-flex align-items-center" style={{padding:'60px 0px'}}>
+						  <div className="col-lg-6 company-top-text">
 							  <p>About ENTER</p>
 							  <p className="title mb-5">더 나은 투자<br/>더 빠른 성장</p>
 							  <p className="sub">초기 스타트업의 안전하고 빠른 성장을 위한 서비스 제공을 넘어<br/>기존의 고인 투자 문화를 타파하는 기업이 되겠습니다.</p>
@@ -26,7 +26,7 @@ function HomeService(){
 					<p className="sub-title">
 						ENTER
 					</p>
-					<p className="sub-content">
+					<p className="sub-content-enter">
 						ENTER는 대표를 포함해 6명의 팀원이 협력하여 서비스를 만들어나가는 기업입니다.<br/>
 						최초로 런칭한 서비스인 Shift는 현재 초기기업 10곳을 모집하여 스크리닝을 통해 걸러진 4곳의 스타트업과 서비스 계약을 진행중입니다.<br/>
 						스타트업이 안전하고 빠르게 성장할 수 있도록, 투자자들이 좋은 기업을 찾을 수 있도록 징검다리 역할을 할 수 있도록 하겠습니다.
@@ -36,7 +36,7 @@ function HomeService(){
 					</div>
 				
 			</div>
-			<div className="container-fluid">
+			<div className="container-fluid company-vision-wrap">
 				<div className="container upper-vision">
 					<img className="opinion-title" src="..\img\Company_vision.png" alt=""/>
 					<div className="company-vision">
@@ -47,7 +47,7 @@ function HomeService(){
 					<div className="row">
 						<div className="col-lg-5 mobile-nopad">
 							<div className="company-box">
-								<img className="opnion-icon" src="..\img\Company_vision.png" alt=""/>
+								<img className="opinion-icon" src="..\img\Company_vision.png" alt=""/>
 								<div className="box-content mb-lg-5 mb-0">
 									투자자들에게 성장 가능성이 있는 스타트업을 직접 연결해줌으로써, 기존 네트워크 안
 									에서만 활동하는 투자자들이 놓칠 수 있는 투자상품 및 서비스를 알립니다.
@@ -63,7 +63,7 @@ function HomeService(){
 						<div className="col-lg-6 position-rewind mobile-nopad">
 							<div className="col-xl-10">
 								<div className="company-box mobile-nomar">
-									<img className="opnion-icon" src="..\img\Company_vision.png" alt=""/>
+									<img className="opinion-icon" src="..\img\Company_vision.png" alt=""/>
 									<div className="box-content mb-lg-5 mb-4">
 										극초기 스타트업은 실패 가능성도 높고, 시간과 자본 등이 부족하여 투자받기 힘듭니다.
 										투자를 받음으로써 성장이 가능한데, 투자를 위해서는 성장을 해야하는 모순이 발생합니다.
@@ -81,7 +81,7 @@ function HomeService(){
 							
 							<div className="col-xl-8 col-lg-10 mt-5">
 								<div className="company-box">
-									<img className="opnion-icon" src="..\img\Company_vision.png" alt=""/>
+									<img className="opinion-icon" src="..\img\Company_vision.png" alt=""/>
 									<div className="box-content mb-lg-5 mb-0">
 										극초기 스타트업의 시작과 성장을 ENTER에서 할 수 있도록,<br/>
 										엑셀러레이팅 서비스의 대명사가 되겠습니다.
@@ -96,16 +96,14 @@ function HomeService(){
 							</div>
 						</div>
 					</div>
-					<div></div>
-					<div></div>
-					<div></div>
+
 				</div>
 			</div>
-			<div className="container">
+			<div className="container" style={{marginTop:'120px'}}>
 				<p className="sub-title mb-lg-3 mb-5">Company Detail</p>
 				<div className="row">
 					<div className="col-lg-6">
-					<img src="../img\회사소개페이지 그림\startup-593304_1920.jpg" alt=""/>
+					<img src="../img\회사소개페이지 그림\startup-593304_1920.jpg" style={{height: '480px', width: '100%', objectFit: 'cover'}} alt=""/>
 					</div>
 					<div className="col-lg-6 d-flex align-items-center mt-lg-0 mt-3">
 						<div>
@@ -117,18 +115,18 @@ function HomeService(){
 							이러한 스타트업을 투자자들과 연결해주고,<br/> 다른 기업들과 협업관계를 가질 수 있
 							도록 지원해줌으로써<br/> 투자 네트워크를 형성해줍니다.
 						</p>
-						<a href="">More Detail {'>'}</a>
+						<a style={{color: '#17375e',fontWeight:'700'}}>More Detail {'>'}</a>
 						</div>
 						
 					</div>
-					<div className="row">
+					<div className="row" style={{marginTop:'120px'}}>
 						<div className="col-lg-6 sub-subtitle detail d-flex align-items-center mb-lg-0 mb-5">
 							Company's <br/>promotion Phrase
 						</div>
 						<div className="col-lg-6 sub-content d-flex align-items-center lh">단순 스타트업 지원 서비스를 제공하는 회사가 아닌<br/>
 						한국의 실리콘밸리가 되겠습니다.</div>
 					</div>
-					<img className="mt-5" src="../img\회사소개페이지 그림\san-francisco-1258848_1920.jpg" alt=""/>
+					<img className="mt-5 company-promotion-phase" src="../img\회사소개페이지 그림\san-francisco-1258848_1920.jpg" alt=""/>
 					
 				</div>
 			</div>
